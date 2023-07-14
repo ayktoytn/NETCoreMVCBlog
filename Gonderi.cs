6b6c6aa8a654ea60779b40fee68f9fde00e91fbb
ayktoytn <38761448+ayktoytn@users.noreply.github.com>
2023-07-14 22:02:45 +0300

    Gonderi.cs
    Gonderi class'ı oluşturuldu.

---
+++ Gonderi.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NETCoreMVCBlog.Models
+{
+    public class Gonderi
+    {
+        public int GonderiId { get; set; }
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+        public DateTime Tarih { get; set; }
+    }
+}
