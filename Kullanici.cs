df91c907014cedfb09cf6d7b9dc28682a6d98124
ayktoytn <38761448+ayktoytn@users.noreply.github.com>
2023-07-14 22:01:28 +0300

    Kullanici.cs
    Kullanıcı bilgileri içerek kullanici.cs classı oluşturuldu

---
+++ Kullanici.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NETCoreMVCBlog.Models
+{
+    public class Kullanici
+    {
+        public int KullaniciId { get; set; }
+        public string KullaniciAdi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+    }
+}
